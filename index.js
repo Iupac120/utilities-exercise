7a4d2307a92e328493d6aa0713ccd0c8a5a4f880
Iupac120 <oforokpara@gmail.com>
2022-05-12 16:11:46 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+//solution  to first file in utilities folder
+const firstModule = require('./utilities/util-1');
+const {cut3} = require('./utilities/util-1')
+const arrayNum = [8,6,4,2,5,9]
+const largestNum = firstModule.largeNumber(arrayNum)
+const cut = cut3(arrayNum)
+
+
+console.log(largestNum)
+console.log(cut)
+
+//solution to second file in utilities folder
+const {sum,concat} = require('./utilities/util-2') 
+const firstArray = [2,4,9,12]
+const secondArray = [0,8,-3,2]
+const addTotal = sum(firstArray)
+console.log(addTotal)
+console.log(concat(firstArray,secondArray))
+
+const secondModule = require('./utilities/util-2')
+console.log(secondModule.sum(secondArray))
+console.log(secondModule.concat(firstArray,secondArray))
 
